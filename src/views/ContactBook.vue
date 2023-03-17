79ea3c181117aa73c2caddd16caf5c93cf0f01c0
mrhaomp2001 <huynhnhuthao248@gmail.com>
2023-03-17 13:10:47 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page row ">
+  <div class="page row">
     <div class="col-md-12"><InputSearch v-model="searchText" /></div>
     <div class="mt-3 col-md-6">
       <h4>
@@ -20,7 +20,17 @@
           Chi tiết Liên hệ
           <i class="fas fa-address-card"></i>
         </h4>
+
         <ContactCard :contact="activeContact" />
+        
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning"> <i class="fas fa-edit"></i> Hiệu chỉnh</span>
+        </router-link>
       </div>
     </div>
   </div>
