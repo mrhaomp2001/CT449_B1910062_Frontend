2367c67d6db5c6b44233611a6a3f86aed3a3ff97
mrhaomp2001 <huynhnhuthao248@gmail.com>
2023-03-22 14:20:15 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,23 @@ const routes = [
         component: ContactBook,
     },
     {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true, // Truyền các biến trong $route.params vào làm props
+    },
+    {
+        path: "/contacts/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        // props: true,
+    },
+    {
         path: "/:pathMatch(.*)*",
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
-    {
-        path: "/contacts/:id",
-        name: "contact.edit",
-        component: () => import("@/views/ContactEdit.vue"),
-        props: true // Truyền các biến trong $route.params vào làm props
-    },
+
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
